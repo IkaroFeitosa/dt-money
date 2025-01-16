4de8d72eef0e751b654bd0ed4ebbf88b1a107366
Ikaro Feitosa <ikaro.feitosa.ext@espm.br>
2025-01-16 16:09:58 -0300

    feat: new transaction modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,25 @@
-import { HeaderContainer, HeaderContent, NewTransactionsButton } from "./styles";
-import LogoImg from "../../assets/logo.svg"
+import {
+  HeaderContainer,
+  HeaderContent,
+  NewTransactionsButton,
+} from "./styles";
+import LogoImg from "../../assets/logo.svg";
+import * as Dialog from "@radix-ui/react-dialog";
+import { NewTransactionModal } from "../NewTransactionModal";
 
-export function Header(){
-    return(
-        <HeaderContainer>
-            <HeaderContent>
-                <img src={LogoImg} alt=""/>
-                <NewTransactionsButton>Nova transação</NewTransactionsButton>
-            </HeaderContent>
-        </HeaderContainer>
-    )
+export function Header() {
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={LogoImg} alt="" />
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionsButton>Nova transação</NewTransactionsButton>
+          </Dialog.Trigger>
+          <NewTransactionModal />
+        </Dialog.Root>
+      </HeaderContent>
+    </HeaderContainer>
+  );
 }
---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,37 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import { CloseButton, Content, Overlay, TransactionTypeButton, TransactionTypeContainer } from "./styles";
+import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
+
+
+export function NewTransactionModal(){
+    return(
+        <Dialog.Portal>
+            <Overlay />
+            <Content>
+              <Dialog.Title>Nova transação</Dialog.Title>
+              <CloseButton>
+                <X size={24}/>
+              </CloseButton>
+              <form action="">
+                <input type="text" placeholder="Descrição" required/>
+                <input type="number" placeholder="Preço" required/>
+                <input type="date" placeholder="Categoria" required/>
+
+                <TransactionTypeContainer>
+                    <TransactionTypeButton variant="income">
+                        <ArrowCircleUp size={24} />
+                        Entrada
+                    </TransactionTypeButton>
+                    <TransactionTypeButton variant="outcome">
+                        <ArrowCircleDown size={24} />
+                        Saída
+                    </TransactionTypeButton>
+                </TransactionTypeContainer>
+
+                <button type="submit">Cadastrar</button>
+              </form>
+              
+            </Content>
+          </Dialog.Portal>
+    )
+}

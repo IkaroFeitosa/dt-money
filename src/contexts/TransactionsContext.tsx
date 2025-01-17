36e620d6f3e98e387c78186cd43111b5cbbcdede
Ikaro Feitosa <ikaro.feitosa.ext@espm.br>
2025-01-17 17:25:50 -0300

    feat: intergation search transaction

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -9,6 +9,7 @@ export interface Transaction {
 }
 interface TransactionContextType {
   transactions: Transaction[];
+  fetchTransactions: (query?: string) => Promise<void>;
 }
 export const TransactionsContext = createContext({} as TransactionContextType);
 
@@ -17,16 +18,20 @@ interface ProviderTransactionProps {
 }
 export function TransactionsProvider({ children }: ProviderTransactionProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
-  async function loadTransactions() {
-    const response = await fetch("http://localhost:3000/transactions");
+  async function fetchTransactions(query?: string) {
+    const url = new URL("http://localhost:3000/transactions");
+    if (query) {
+      url.searchParams.set("q", query);
+    }
+    const response = await fetch(url);
     const data = await response.json();
     setTransactions(data);
   }
   useEffect(() => {
-    loadTransactions();
+    fetchTransactions();
   }, []);
   return (
-    <TransactionsContext.Provider value={{ transactions }}>
+    <TransactionsContext.Provider value={{ transactions, fetchTransactions }}>
       {children}
     </TransactionsContext.Provider>
   );

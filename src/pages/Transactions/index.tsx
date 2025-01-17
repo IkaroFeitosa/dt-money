f4d369e9213ebc6caa706aa7334e82d93f08dd9d
Ikaro Feitosa <ikaro.feitosa.ext@espm.br>
2025-01-16 23:07:05 -0300

    feat: values formatter

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -8,10 +8,10 @@ import {
   TransactionsTable,
 } from "./styles";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
-
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
 
 export function Transactions() {
-  const {transactions} = useContext(TransactionsContext)
+  const { transactions } = useContext(TransactionsContext);
   return (
     <div>
       <Header />
@@ -24,10 +24,14 @@ export function Transactions() {
               <tr key={transaction.id}>
                 <td width={"50%"}>{transaction.description}</td>
                 <td>
-                  <PriceHighlight variant={transaction.type}>{transaction.price}</PriceHighlight>
+                  <PriceHighlight variant={transaction.type}>
+                    {transaction.type === "outcome" && "-"}
+                    {' '}
+                    {priceFormatter.format(transaction.price)}
+                  </PriceHighlight>
                 </td>
                 <td>{transaction.category}</td>
-                <td>{transaction.createdAt}</td>
+                <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
               </tr>
             ))}
           </tbody>

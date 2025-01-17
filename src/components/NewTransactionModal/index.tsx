664ee53cd67d08f638e4f9987a2adf9070fdff02
Ikaro Feitosa <ikaro.feitosa.ext@espm.br>
2025-01-17 17:08:26 -0300

    feat: react-hook-forms and Controlled components

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,37 +1,89 @@
 import * as Dialog from "@radix-ui/react-dialog";
-import { CloseButton, Content, Overlay, TransactionTypeButton, TransactionTypeContainer } from "./styles";
+import {
+  CloseButton,
+  Content,
+  Overlay,
+  TransactionTypeButton,
+  TransactionTypeContainer,
+} from "./styles";
 import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
+import * as zod from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, useForm } from "react-hook-form";
 
+const newTransactionFormSchema = zod.object({
+  description: zod.string(),
+  price: zod.number(),
+  category: zod.string(),
+  type: zod.enum(["income", "outcome"]),
+});
+type NewTransactionFormInputs = zod.infer<typeof newTransactionFormSchema>;
 
-export function NewTransactionModal(){
-    return(
-        <Dialog.Portal>
-            <Overlay />
-            <Content>
-              <Dialog.Title>Nova transação</Dialog.Title>
-              <CloseButton>
-                <X size={24}/>
-              </CloseButton>
-              <form action="">
-                <input type="text" placeholder="Descrição" required/>
-                <input type="number" placeholder="Preço" required/>
-                <input type="date" placeholder="Categoria" required/>
+export function NewTransactionModal() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+    control,
+  } = useForm<NewTransactionFormInputs>({
+    resolver: zodResolver(newTransactionFormSchema),
+  });
+  async function handleCreateTransaction(data: NewTransactionFormInputs) {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.log(data);
+  }
+  return (
+    <Dialog.Portal>
+      <Overlay />
+      <Content>
+        <Dialog.Title>Nova transação</Dialog.Title>
+        <CloseButton>
+          <X size={24} />
+        </CloseButton>
+        <form action="" onSubmit={handleSubmit(handleCreateTransaction)}>
+          <input
+            type="text"
+            {...register("description")}
+            placeholder="Descrição"
+            required
+          />
+          <input
+            type="number"
+            {...register("price", {
+              valueAsNumber: true,
+            })}
+            placeholder="Preço"
+            required
+          />
+          <input
+            type="text"
+            {...register("category")}
+            placeholder="Categoria"
+            required
+          />
 
-                <TransactionTypeContainer>
-                    <TransactionTypeButton variant="income" value="income">
-                        <ArrowCircleUp size={24} />
-                        Entrada
-                    </TransactionTypeButton>
-                    <TransactionTypeButton variant="outcome" value="outcome">
-                        <ArrowCircleDown size={24} />
-                        Saída
-                    </TransactionTypeButton>
-                </TransactionTypeContainer>
+          <Controller
+            control={control}
+            name="type"
+            render={({field}) => (
+              <TransactionTypeContainer value={field.value} onValueChange={field.onChange}>
+                <TransactionTypeButton variant="income" value="income">
+                  <ArrowCircleUp size={24} />
+                  Entrada
+                </TransactionTypeButton>
+                <TransactionTypeButton variant="outcome" value="outcome">
+                  <ArrowCircleDown size={24} />
+                  Saída
+                </TransactionTypeButton>
+              </TransactionTypeContainer>
+            )}
+          />
 
-                <button type="submit">Cadastrar</button>
-              </form>
-              
-            </Content>
-          </Dialog.Portal>
-    )
+          <button type="submit" disabled={isSubmitting}>
+            Cadastrar
+          </button>
+        </form>
+      </Content>
+    </Dialog.Portal>
+  );
 }
--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,10 +1,29 @@
+import { useForm } from "react-hook-form";
 import { SearchFormContainer } from "./styles";
+import * as zod from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-export function SearchForm(){
-    return(
-        <SearchFormContainer>
-            <input type="text" placeholder="Buscar transações"/>
-            <button>Pesquisar</button>
-        </SearchFormContainer>
-    )
+const searchFormSchema = zod.object({
+  query: zod.string(),
+});
+type SearchFormInputs = zod.infer<typeof searchFormSchema>;
+
+export function SearchForm() {
+  const { register, handleSubmit, formState:{isSubmitting} } = useForm<SearchFormInputs>({
+    resolver: zodResolver(searchFormSchema),
+  });
+  async function handleSearchTransactions(data: SearchFormInputs) {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.log(data);
+  }
+  return (
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+      <input
+        type="text"
+        {...register("query")}
+        placeholder="Buscar transações"
+      />
+      <button type="submit" disabled={isSubmitting}>Pesquisar</button>
+    </SearchFormContainer>
+  );
 }
